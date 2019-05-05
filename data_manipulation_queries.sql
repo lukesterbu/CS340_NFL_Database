c5fc205268e946bcbefe0f4b0618516c198cf117
Eli Koslofsky <elibk91@outlook.com>
2019-05-05 16:55:16 -0400

    added better comment to player query search

--- data_manipulation_queries.sql
+++ data_manipulation_queries.sql
@@ -177,6 +177,7 @@ SET    firstname = :firstname,
 WHERE  playerid = :playerID; 
 
 -- query to search for player by first or last name
+-- with colon : character being used to denote the variables that will have data from the backend programming 
 select * from player
 WHERE firstName LIKE CONCAT ('%', :firstNameSearchTerm, '%')
 OR lastName LIKE CONCAT ('%', :lastNameSearchTerm, '%');
