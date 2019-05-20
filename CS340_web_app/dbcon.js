dc6939a7eb0a474e7651c9f6fbb11ab14dc9bace
Luke <burrisl@oregonstate.edu>
2019-05-20 15:46:10 +0100

    Update dbcon.js

--- CS340_web_app/dbcon.js
+++ CS340_web_app/dbcon.js
@@ -3,7 +3,7 @@ var pool = mysql.createPool({
   connectionLimit : 10,
   host            : 'classmysql.engr.oregonstate.edu',
   user            : 'cs340_burrisl',
-  password        : 'Maggiestation21',
+  password        : '5665',
   database        : 'cs340_burrisl'
 });
 
